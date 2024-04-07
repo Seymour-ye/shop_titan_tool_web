5ffe6594ac9b1d77882cdbbd0a7313a0e75ed8a8
Seymour-ye <zhimo.ye@hotmail.com>
2024-04-07 05:08:20 -0400

    updated i18n configurations, created home page for the app

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module ShopTitanToolWeb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.available_locales = %i[en zh]
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
