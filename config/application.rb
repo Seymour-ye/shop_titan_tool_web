4f201f8794d4ec5e7261cfda9d3c77ed6f20fd35
Seymour-ye <zhimo.ye@hotmail.com>
2024-04-07 15:24:14 -0400

    favicon changed, several minor changes

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module ShopTitanToolWeb
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.i18n.available_locales = %i[en zh]
+    config.i18n.available_locales = [:en, :zh]
     config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  get '/calendar', to: 'calendar#index'
-  get 'static_pages/home'
-  resources :events
-  resources :components
-  resources :quests
-  resources :workers
-  resources :blueprints
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    get '/calendar', to: 'calendar#index'
+    get 'static_pages/home'
+    resources :events
+    resources :components
+    resources :quests
+    resources :workers
+    resources :blueprints
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "static_pages#home"
+    # Defines the root path route ("/")
+    # root "articles#index"
+    root "static_pages#home"
 end
