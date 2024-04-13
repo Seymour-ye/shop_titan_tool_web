e6accd268a7c10d48fc140a42e6e7ddebf312e8d
william hancharek <4767506+williamhancharek@users.noreply.github.com>
2024-04-12 23:04:52 -0400

    add button to change languages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,18 @@
 class ApplicationController < ActionController::Base
-    around_action :switch_locale
+  around_action :switch_locale
 
-    private
+  private
 
-    def switch_locale(&action)
-      logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-      locale = params[:locale] || extract_locale_from_accept_language_header
-      logger.debug "* Locale set to '#{locale}'"
-      I18n.with_locale(locale, &action)
-    end
+  def switch_locale(&action)
+    locale = session[:locale] || extract_locale_from_accept_language_header
+    I18n.with_locale(locale, &action)
+  end
 
-    def extract_locale_from_accept_language_header
-      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
-    end
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
 
-    def default_url_options
-        { locale: I18n.locale }
-    end
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
---
+++ app/controllers/languages_controller.rb
@@ -0,0 +1,7 @@
+class LanguagesController < ApplicationController
+  def update
+    session[:locale] = params[:id]
+
+    redirect_back fallback_location: root_path
+  end
+end
