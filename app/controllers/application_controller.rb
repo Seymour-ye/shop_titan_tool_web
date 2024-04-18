560b48355b3b6c16a02178040ed7d40ca4c8674e
Seymour-ye <zhimo.ye@hotmail.com>
2024-04-18 00:38:39 -0400

    Bugfix: handling browser default locales that are not supported

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,13 @@ class ApplicationController < ActionController::Base
   private
 
   def switch_locale(&action)
-    locale = session[:locale] || extract_locale_from_accept_language_header
+    locale = session[:locale] || extract_locale_from_accept_language_header || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 
   def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    browser_default = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    I18n.available_locales.include?(browser_default.to_sym) ? browser_default : I18n.default_locale
   end
 
   # def default_url_options
